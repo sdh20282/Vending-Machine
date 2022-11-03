ca16a71a619321c235a795633269a1bf980d4a9f
donghyeon.seo <82585734+sdh20282@users.noreply.github.com>
2022-11-03 18:10:07 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -168,7 +168,7 @@ function generateListItem(productName, productCount, productImgPath) {
     figure.classList.add('selected_product_image');
     figure.appendChild(img);
     figure.appendChild(figcaption);
-    img.setAttribute('src', `/assets/images/${productImgPath}`);
+    img.setAttribute('src', `./assets/images/${productImgPath}`);
     img.setAttribute('alt', `음료 이미지입니다.`);
     figcaption.appendChild(desc);
     desc.classList.add('product_name');
